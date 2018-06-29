7df6d3e3d0320458082da58535289674a1e3724f
Wilfred Furthado M <wilfred.furthado@pervacio.com>
2018-06-29 14:43:02 +0530

    Added ACDCUtilities class

--- ACDCSwift/Supporting Files/ACDCConstants.swift
+++ ACDCSwift/Supporting Files/ACDCConstants.swift
@@ -7,29 +7,3 @@
 //
 
 import Foundation
-import UIKit
-
-class ACDCCommonMethods {
-    static func showMessage(title: String, msg: String) {
-        let alert = UIAlertController(title: title, message: msg, preferredStyle: UIAlertControllerStyle.alert)
-        alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.default, handler: nil))
-        alert.addAction(UIAlertAction(title: "Confirm", style: UIAlertActionStyle.default, handler: nil))
-        UIApplication.topViewController()?.present(alert, animated: true, completion: nil)
-    }
-}
-
-extension UIApplication {
-    static func topViewController(base: UIViewController? = UIApplication.shared.delegate?.window??.rootViewController) -> UIViewController? {
-        if let nav = base as? UINavigationController {
-            return topViewController(base: nav.visibleViewController)
-        }
-        if let tab = base as? UITabBarController, let selected = tab.selectedViewController {
-            return topViewController(base: selected)
-        }
-        if let presented = base?.presentedViewController {
-            return topViewController(base: presented)
-        }
-        return base
-    }
-}
-
